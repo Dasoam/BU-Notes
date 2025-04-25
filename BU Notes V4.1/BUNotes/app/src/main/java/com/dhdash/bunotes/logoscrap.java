306852d99b890f4271d9571bbcf3348ee27eb6e7
Dasoam <140055180+Dasoam@users.noreply.github.com>
2025-04-25 15:25:32 +0530

    V 4.1

---
+++ BU Notes V4.1/BUNotes/app/src/main/java/com/dhdash/bunotes/logoscrap.java
@@ -0,0 +1,36 @@
+package com.dhdash.bunotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class logoscrap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
+
+        setContentView(R.layout.activity_logoscrap);
+        loding();
+    }
+    private void loding(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), loginsingup.class));
+                finish();
+            }
+        },2000);
+    }
+}
